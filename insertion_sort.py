ac01bc418df9812206747b292a206eb56a5f49c3
psych831 <psych831@gmail.com>
2020-07-15 13:50:21 +0530

    Insertion Sort
    Finished insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,2 +1,14 @@
 def insert_sort(a):
-    
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j-1
+        while i >= 0 and a[i] > key:
+            a[i+1] = a[i]
+            i = i-1
+        a[i+1] = key
+    return a
+
+
+a = [6, 5, 4, 7, 1]
+b = insert_sort(a)
+print(b)
